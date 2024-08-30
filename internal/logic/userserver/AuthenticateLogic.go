24d9ac771eeeab2f45f038a82cffc6df0f7edd8c
zip <zzp-email@qq.com>
2024-08-30 15:58:17 +0800

    验证密码

--- internal/logic/userserver/AuthenticateLogic.go
+++ internal/logic/userserver/AuthenticateLogic.go
@@ -2,6 +2,10 @@ package userserverlogic
 
 import (
 	"context"
+	"fmt"
+	"github.com/zzp-Z/UserServer/db/crud"
+	"github.com/zzp-Z/UserServer/internal/logic"
+	"github.com/zzp-Z/UserServer/logs"
 
 	"github.com/zzp-Z/UserServer/internal/svc"
 	"github.com/zzp-Z/UserServer/user_server"
@@ -13,19 +17,45 @@ type AuthenticateLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 	logx.Logger
+	UserModel crud.UserModel
+	Tools     *logic.Tools
 }
 
 func NewAuthenticateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AuthenticateLogic {
 	return &AuthenticateLogic{
-		ctx:    ctx,
-		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
+		ctx:       ctx,
+		svcCtx:    svcCtx,
+		Logger:    logx.WithContext(ctx),
+		UserModel: crud.NewUserModel(svcCtx.SqlConn, svcCtx.CacheConf),
+		Tools:     &logic.Tools{},
 	}
 }
 
-// 验证用户密码（用途包括不限于“登录”，修改密码前的验证，修改信息前的验证等）
+// Authenticate 验证用户密码（用途包括不限于“登录”，修改密码前的验证，修改信息前的验证等）
 func (l *AuthenticateLogic) Authenticate(in *user_server.AuthenticateRequest) (*user_server.AuthenticateResponse, error) {
-	// todo: add your logic here and delete this line
+	// Step 1: 验证邮箱是否存在
+	user, err := l.UserModel.FindOneByEmail(l.ctx, in.Email)
+	if err != nil {
+		logs.Error(nil, logs.ErrorContent{
+			Message:   in.Email,
+			Error:     err,
+			ErrorCode: "AA900",
+		})
+		return nil, fmt.Errorf("邮箱不存在")
+	}
+	// Step 2: 验证密码是否正确
+	isValid := l.Tools.CheckPassword(in.Password, user.HashPassword)
+	if !isValid {
+		err = fmt.Errorf("密码错误")
+		logs.Error(nil, logs.ErrorContent{
+			Message:   in.Email,
+			Error:     err,
+			ErrorCode: "AA901",
+		})
+		return nil, err
+	}
 
-	return &user_server.AuthenticateResponse{}, nil
+	return &user_server.AuthenticateResponse{
+		UserId: uint32(user.Id),
+	}, nil
 }
